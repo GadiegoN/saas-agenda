3e13716cbb52fe69037c7724d032335b12473f5a
GadiegoN <ngadiego@gmail.com>
2025-05-30 23:11:54 -0300

    feat: add SignOutButton component and integrate it into the dashboard; enhance authentication page session handling

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,10 +1,25 @@
-export default function DashboardPage() {
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { SignOutButton } from "./_components/sign-out-button";
+
+export default async function DashboardPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/authentication");
+  }
+
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <h1 className="text-2xl font-bold">Dashboard</h1>
       <p className="mt-4 text-gray-600">
-        This is a private page, accessible only to authenticated users.
+        {session ? `Bem-vindo, ${session.user.name}!` : "Você não está logado."}
       </p>
+
+      <SignOutButton />
     </div>
   );
 }
--- src/app/(public)/authentication/page.tsx
+++ src/app/(public)/authentication/page.tsx
@@ -1,8 +1,19 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { FormLogin } from "./_components/form-login";
 import { FormRegister } from "./_components/form-register";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function AuthenticationPage() {
+export default async function AuthenticationPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session?.user) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <Tabs defaultValue="login" className="w-[400px]">
