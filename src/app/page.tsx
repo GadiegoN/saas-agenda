029a8078cc2645e69f78294889ab7ab83d910af1
GadiegoN <ngadiego@gmail.com>
2025-05-30 22:52:23 -0300

    feat: enhance registration error handling with user feedback and update layout structure on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
       <h1>Ola mundo!</h1>
       <Button>Ola</Button>
     </div>
