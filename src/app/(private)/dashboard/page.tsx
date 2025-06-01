cd6e5f73ce5aced0231d1e1f358d7c2bf35c5853
GadiegoN <ngadiego@gmail.com>
2025-06-01 17:40:40 -0300

    feat(dashboard): add appointments chart and stats cards, implement date picker, and enhance dashboard layout
    - Introduced AppointmentsChart component for visualizing daily appointments and revenue.
    - Added StatsCards component to display total revenue, appointments, patients, and doctors.
    - Implemented DatePicker component for selecting date ranges.
    - Removed obsolete SignOutButton component.
    - Created TopDoctors and TopSpecialties components to showcase top-performing doctors and specialties.
    - Updated dashboard page to integrate new components and improve layout.
    - Added progress bar component for visual representation of data.
    - Enhanced global styles and layout with new font and theme adjustments.
    - Updated data fetching logic for dashboard metrics.
    - Localized data table messages to Portuguese.

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,37 +1,120 @@
-import { auth } from "@/lib/auth";
+import dayjs from "dayjs";
+import { Calendar } from "lucide-react";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-import { SignOutButton } from "./_components/sign-out-button";
-import { getInitials } from "@/utils/get-initials";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-export default async function DashboardPage() {
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { DataTable } from "@/components/ui/data-table";
+import {
+  PageActions,
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/layout/page-container";
+import { auth } from "@/lib/auth";
+
+import { appointmentsTableColumns } from "../appointments/_components/table-columns";
+import { DatePicker } from "./_components/date-picker";
+import { getDashboard } from "@/data/get-dashboard";
+import { AppointmentsChart } from "./_components/appoiments-chart";
+import { StatsCards } from "./_components/stats-cards";
+import { TopDoctors } from "./_components/top-doctors";
+import { TopSpecialties } from "./_components/top-specialties";
+
+interface DashboardPageProps {
+  searchParams: Promise<{
+    from: string;
+    to: string;
+  }>;
+}
+
+export default async function Dashboard({ searchParams }: DashboardPageProps) {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-
-  if (!session) {
+  if (!session?.user) {
     redirect("/authentication");
   }
-
-  if (session.user.clinic === null) {
+  if (!session.user.clinic) {
     redirect("/clinic-form");
   }
+  // if (!session.user.plan) {
+  //   redirect("/new-subscription");
+  // }
+  const { from, to } = await searchParams;
+  if (!from || !to) {
+    redirect(
+      `/dashboard?from=${dayjs().format("YYYY-MM-DD")}&to=${dayjs().add(1, "month").format("YYYY-MM-DD")}`,
+    );
+  }
+  const {
+    totalRevenue,
+    totalAppointments,
+    totalPatients,
+    totalDoctors,
+    topDoctors,
+    topSpecialties,
+    todayAppointments,
+    dailyAppointmentsData,
+  } = await getDashboard({
+    from,
+    to,
+    session: {
+      user: {
+        clinic: {
+          id: session.user.clinic.id,
+        },
+      },
+    },
+  });
 
   return (
-    <div className="flex h-screen flex-col">
-      <h1 className="text-2xl font-bold">Dashboard</h1>
-
-      <Avatar className="size-24">
-        <AvatarImage src={session?.user?.image as string} />
-        <AvatarFallback>{getInitials(session?.user?.name)}</AvatarFallback>
-      </Avatar>
-
-      <p className="mt-4 text-gray-600">
-        {session ? `Bem-vindo, ${session.user.name}!` : "Você não está logado."}
-      </p>
-
-      <SignOutButton />
-    </div>
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Dashboard</PageTitle>
+          <PageDescription>
+            Tenha uma visão geral da sua clínica.
+          </PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <DatePicker />
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        <StatsCards
+          totalRevenue={totalRevenue.total ? Number(totalRevenue.total) : null}
+          totalAppointments={totalAppointments.total}
+          totalPatients={totalPatients.total}
+          totalDoctors={totalDoctors.total}
+        />
+        <div className="grid grid-cols-[2.25fr_1fr] gap-4">
+          <AppointmentsChart dailyAppointmentsData={dailyAppointmentsData} />
+          <TopDoctors doctors={topDoctors} />
+        </div>
+        <div className="grid grid-cols-[2.25fr_1fr] gap-4">
+          <Card>
+            <CardHeader>
+              <div className="flex items-center gap-3">
+                <Calendar className="text-muted-foreground" />
+                <CardTitle className="text-base">
+                  Agendamentos de hoje
+                </CardTitle>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <DataTable
+                columns={appointmentsTableColumns}
+                data={todayAppointments}
+              />
+            </CardContent>
+          </Card>
+          <TopSpecialties topSpecialties={topSpecialties} />
+        </div>
+      </PageContent>
+    </PageContainer>
   );
 }
